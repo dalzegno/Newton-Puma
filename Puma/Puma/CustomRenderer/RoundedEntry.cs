c8b195d8016bce62f311daa10aaa4ca03701eea1
dalzegno <victnek@gmail.com>
2022-01-12 15:20:22 +0100

    navbar init styling and Searchbar Custom Renderer

---
+++ Puma/Puma/CustomRenderer/RoundedEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace Client.CustomRenderer
+{
+    public class RoundedEntry : Entry
+    {
+       
+    }
+    
+}
