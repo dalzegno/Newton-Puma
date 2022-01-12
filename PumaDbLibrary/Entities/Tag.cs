f14f96d49aa2b4c042b2cedf81f19d13cbad4b43
wallenas <peter.wallenas@gmail.com>
2022-01-12 11:10:37 +0100

    Mappstruktur, simpel db med lite modeller - OBS: Id är long, borde vara int så småningom

---
+++ PumaDbLibrary/Entities/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace PumaDbLibrary
+{
+    [Table("Tag")]
+    public partial class Tag
+    {
+        public Tag()
+        {
+            PoiTags = new HashSet<PoiTag>();
+        }
+
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(45)")]
+        public string Name { get; set; }
+
+        [InverseProperty(nameof(PoiTag.Tag))]
+        public virtual ICollection<PoiTag> PoiTags { get; set; }
+    }
+}
