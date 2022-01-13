f08c516a0a28c360140dcef89e4f2a434d826248
dalzegno <victnek@gmail.com>
2022-01-13 14:58:03 +0100

    Signup popup + navigation buttons

--- Puma/Puma/Views/MainPage.xaml.cs
+++ Puma/Puma/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -32,6 +33,51 @@ namespace Puma
             System.Diagnostics.Debug.WriteLine($"MapClick: {e.Position.Latitude}, {e.Position.Longitude}");
         }
 
-        
+        private void btn_closePopup(object sender, EventArgs e)
+        {
+            loginPopup.IsVisible = false;
+            signupPopup.IsVisible = false;
+        }
+
+        private void btn_Signup_Popup_Clicked(object sender, EventArgs e)
+        {
+            signupPopupVisible();
+        }
+
+        private void btn_Login_Popup_Clicked(object sender, EventArgs e)
+        {
+            loginPopupVisible();
+        }
+
+        //metoder
+        public void loginPopupVisible()
+        {
+            signupPopup.IsVisible = false;
+            loginPopup.IsVisible = true;
+        }
+        public void signupPopupVisible()
+        {
+            loginPopup.IsVisible = false;
+            signupPopup.IsVisible = true;
+        }
+
+        private void btn_login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_signup_Clicked(object sender, EventArgs e)
+        {
+            var user = new UserDto()
+            {
+                Email = txt_signupEmail?.Text,
+                Password = txt_loginPassword?.Text,
+                DisplayName = txt_signupDisplayName?.Text,
+                FirstName = txt_signupFirstName?.Text ?? "",
+                LastName = txt_signupSurname?.Text ?? ""
+            };
+
+
+        }
     }
 }
