1f79d046565a8200172b8b344beff898905d8212
wallenas <peter.wallenas@gmail.com>
2022-01-20 11:28:03 +0100

    Create/Get Tags

--- Server/Logic/Services/IPoiService.cs
+++ Server/Logic/Services/IPoiService.cs
@@ -13,7 +13,9 @@ namespace Logic.Services
         Task<PointOfInterestDto> CreateAsync(AddPoiDto pointOfInterest);
         Task<PointOfInterestDto> AddCommentAsync(AddCommentDto comment);
         Task<PointOfInterestDto> AddGrade(int poiId, int userId, int gradeType);
+        Task<TagDto> CreateTagAsync(string name);
         Task<PointOfInterestDto> GetAsync(int id);
+        Task<ICollection<TagDto>> GetTagsAsync();
         Task<PointOfInterestDto> UpdateAsync(PointOfInterestDto pointOfInterest);
         Task<PointOfInterestDto> DeleteAsync(int pointOfInterest);
     }
